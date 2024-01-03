100fda3ddb115118ea10cb8c35d30b06f8ad81e8
= <=>
2024-01-03 15:19:40 +0300

    rewritten matrix with templates describing size

--- Matrix.h
+++ Matrix.h
@@ -1,24 +1,78 @@
-#include <vector>
+#pragma once
 
+#include <array>
+
+template <std::size_t N, std::size_t M>
 class Matrix {
 public:
-    explicit Matrix(std::vector<std::vector<double> > v);
-    Matrix(std::size_t n, std::size_t m);
-    Matrix(const Matrix& other);
-    Matrix(Matrix&& other) noexcept;
-    Matrix& operator=(const Matrix& other);
-    Matrix& operator=(Matrix&& other) noexcept;
-    std::vector<double>& operator[](int i);
-    const std::vector<double>& operator[](int i) const;
-    ~Matrix();
+    explicit Matrix(std::array<std::array<double, M>, N> v): data_(v) {}
+    Matrix<N, M>() = default;
+    Matrix<N, M>(const Matrix<N, M>& other) : data_(other.data_) {}
+    Matrix<N, M>(Matrix<N, M>&& other) noexcept : data_(std::forward<std::array<std::array<double, M>, N>>(other.data_)){}
+    Matrix<N, M>& operator=(const Matrix<N, M>& other) {
+        return *this = Matrix(other);
+    }
+    Matrix<N, M>& operator=(Matrix<N, M>&& other) noexcept {
+        Matrix tmp = std::move(other);
+        std::swap(data_, tmp.data_);
+        return *this;
+    }
+    std::array<double, M>& operator[](int i) {
+        return data_[i];
+    }
+    const std::array<double, M>& operator[](int i) const {
+        return data_[i];
+    }
+    ~Matrix<N, M>() {};
 
-    std::vector<std::vector<double> > toVec();
-    std::pair<std::size_t, std::size_t> size() const;
+    std::array<std::array<double, M>, N> toArray() {
+        return data_;
+    }
+    std::pair<std::size_t, std::size_t> size() const {
+        return std::make_pair(N, M);
+    }
 private:
-    std::vector<std::vector<double> > data_;
+    std::array<std::array<double, M>, N> data_ = std::array<std::array<double, M>, N> ();
 };
 
-Matrix operator+(const Matrix& m1, const Matrix& m2);
-Matrix operator-(const Matrix& m);
-Matrix operator-(const Matrix& m1, const Matrix& m2);
-Matrix operator*(const Matrix& m1, const Matrix& m2);
+template <std::size_t N, std::size_t M>
+Matrix<N, M> operator+(const Matrix<N, M> & m1, const Matrix<N, M> & m2) {
+    Matrix sum = Matrix<N, M>();
+    for (std::size_t i = 0; i < N; ++i) {
+        for (std::size_t j = 0; j < M; ++j) {
+            sum[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
+    return sum;
+}
+
+template <std::size_t N, std::size_t M>
+Matrix<N, M> operator-(const Matrix<N, M> &m1, const Matrix<N, M> &m2) {
+    Matrix sub = Matrix<N, M>();
+    for (std::size_t i = 0; i < N; ++i) {
+        for (std::size_t j = 0; j < M; ++j) {
+            sub[i][j] = m1[i][j] - m2[i][j];
+        }
+    }
+    return sub;
+}
+
+template <std::size_t N, std::size_t M>
+Matrix<N, M> operator-(const Matrix<N, M> &m) {
+    return Matrix(m.size().first, m.size().second) - m;
+}
+
+template <std::size_t N, std::size_t M, std::size_t K>
+Matrix<N, K> operator*(const Matrix<N, M> &m1, const Matrix<M, K> &m2) {
+    Matrix mul = Matrix<N, K>();
+    for (std::size_t i = 0; i < N; ++i) {
+        for (std::size_t k = 0; k < K; ++k) {
+            double sum = 0;
+            for (std::size_t j = 0; j < M; ++j) {
+                sum += m1[i][j] * m2[j][k];
+            }
+            mul[i][k] = sum;
+        }
+    }
+    return mul;
+}
--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 
 int main() {
     ActivateFunction f = ActivateFunction();
-    std::cout << f.evaluate0(5) << std::endl;
-    Matrix m({{1, 2}, {3, 4}});
-    Matrix m2 = m * m;
-    for (int i = 0; i < 2; ++i) {
+    std::cout << f.evaluate0(5) << " " << f.evaluate1(5) << std::endl;
+    Matrix<2, 2> m({{{1, 2}, {3, 4}}});
+    Matrix<3, 1> m1({{{1}, {2}, {3}}});
+    Matrix<1, 2> m2({1, 2});
+    auto r = m1 * m2;
+    std::cout << r.size().first << " " << r.size().second << std::endl;
+    for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 2; ++j) {
-            std::cout << m2[i][j] << " ";
+            std::cout << r[i][j] << " ";
         }
         std::cout << std::endl;
     }
